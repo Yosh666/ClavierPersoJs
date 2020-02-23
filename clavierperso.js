f9f1870f0b833a5f0c5cc2cc84994f2eb0bb94de
Yosh666 <audrey1cauvin@gmail.com>
2020-02-23 15:28:31 +0100

    réécriture clavier terminé avec commentaire du cours

--- clavierperso.js
+++ clavierperso.js
@@ -1,11 +1,34 @@
+//1 on explose les lettres dans un tableau
 let lettres="AZERTYUIOPQSDFGHJKLMWXCVBN";
 array_lettres= lettres.split('');//mets les lettres dans un tableau implode sort les lettres d'un tableau (attention implode fonction php a voir en js le bon lexique)
-console.log (array_lettres);//verification a virer par la suite
+//console.log (array_lettres);//verification a virer par la suite
+
+
+/*3 on fait le document ready qui se trouve avant la création de touches 
+mais c'est parce que le document ready et les fonction affiliées sont toujours en haut */
+$(document).ready(function(){//tjr des parenthèses même vides aprés une function
+//document ready c'est quand le document vient de finir de charger.
+ 
+    $(".touche").click(function(){
+        $(".touche").removeClass ("click");//on enlève la class click de toutes les touches
+        $(this).addClass ("click");//$(this)cible la touche particulière qui vient d'être cliquée
+        lettre = $(this).text();//on cible la lettre particulière attribué a cette touche par le split en tableau
+/*!!!!! pensez à regarder ici pour le pendu!*/
+        $("#screen").append (lettre);//on ajoute la lettre de la touche ciblée a la suite du texte déja écrit c'est synchrone
+    
+    })//tjrs une parenthése fermante derrière l'accolade fermante de la function sinon c kil ya un bug
+
+    $(".touche.clear").click (function(){
+        $("#screen").text("");//on ecrit du texte vide
+/*!!!! se renseigner sur la propriété text pour le pendu*/
+    })
+
+
+})
 
 
 
-
-
+//2on crée les touches et on leur donne une classe pour pouvoir les cibler
 /*portion pour créer les touches*/
 ligne=$('#clavier').children('.ligne')[0];//faire d'une classe une variable en commençant par la première div
 $ligne= $(ligne);// on crée la première ligne pour rentrer les lettres
@@ -17,4 +40,5 @@ for (let i=0;i<array_lettres.lengh;i++){//boucle pour faire les 26 touches
         $ligne=$ligne.next();//sauter a la prochaine ligne la "next" div koi
     }
 }
-
+$ligne.append("<div class='touche clear'>Clear</div>");/*on rajoute la touche clear en fin de la dernier variable $(ligne)
+$(ligne)étant la classe devenue variable*/ 
